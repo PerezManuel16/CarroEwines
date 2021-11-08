4d5d6a5bf09f4a3dfd76f39594d2fd3e4b036dd3
JuanManuel <perez.manuel16@gmail.com>
2021-11-08 16:25:45 -0300

    cards

--- ewines/src/components/Catalogo.jsx
+++ ewines/src/components/Catalogo.jsx
@@ -1,24 +1,30 @@
+import {Component} from 'react';
 
-
-const Catalogo = () =>{
-    return ( 
-    
-    
-        <div className="card">
-            <img src="..." className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">Card title</h5>
-                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
+class Catalogo  extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            id: 1,
+            vino: 'Trumpeter',
+            uva: 'Malbec',
+            detalle:'Some quick example text to build on the card title and make up the bulk of the cards content.',
+            img:'https://rinconelegido.com.ar/wp-content/uploads/2017/08/Art10087_TrumpeterMalbec_750cc.jpg'
+        }
+        
+    }
+    render() { 
+        return ( 
+            <div className="card">
+                <img src={this.state.img} className="card-img-top" alt=""/>
+                <div className="card-body">
+                    <h5 className="card-title">{this.state.vino}</h5>
+                    <p className="card-text">{this.state.uva}</p>
+                    <p className="card-text">{this.state.detalle}</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
             </div>
-        </div>
-    
-
-
-
-    )
-
-
-};
+        );
+    }
+}
 
 export default Catalogo;
